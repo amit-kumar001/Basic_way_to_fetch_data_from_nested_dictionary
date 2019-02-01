545441c66c8b41ef56b2e791857ca51be3c961c1
amit-kumar001 <47202519+amit-kumar001@users.noreply.github.com>
2019-02-01 17:32:38 +0530

    Added some comments

--- n_dict.py
+++ n_dict.py
@@ -1,5 +1,7 @@
-n_dict={'a':{'b':[1,2,3]}}
-print(n_dict)
+# n_dict is the name of dictionary where 'a' is the (key) and ('b':[1,2,3]) is the value of dictionary n_dict.
+
+n_dict={'a':{'b':[1,2,3]}} # value of 'b' is in list and hence we can go for indexing also
+#n_dict is a nested dictionary)
 
 # First way to fetch single value from nested dictionary by using print statement and using keys
 print(n_dict['a']['b'][0])
