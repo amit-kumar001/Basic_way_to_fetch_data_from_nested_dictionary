82a38d2f24e670fc1e1b58f62b958037fa455a22
amit-kumar001 <47202519+amit-kumar001@users.noreply.github.com>
2019-02-01 16:46:48 +0530

    Update n_dict.py

--- n_dict.py
+++ n_dict.py
@@ -5,7 +5,7 @@ print(n_dict)
 print(n_dict['a']['b'][0])
 
 # second way to fetch single value from nested dictionary by using get method defined in dictionary
-n=n_dict.get("a",{}).get("b",{})[0]
+n=n_dict.get("a").get("b")[0]
 print(n)
 
 #third way to fetch single value from nested dictionary by using for loop and values() method defined in dictionary
